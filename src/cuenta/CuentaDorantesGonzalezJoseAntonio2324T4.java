1550ac954f00064030ed4ce3b3f78891635ab5af
Josan Dorantes <Josan Dorantes@DESKTOP-OM9NTG9>
2024-03-06 23:33:32 +0100

    Mi proyecto DorantesGonzalezJoseAntonio2324 comentado con javadoc

--- src/cuenta/CuentaDorantesGonzalezJoseAntonio2324T4.java
+++ src/cuenta/CuentaDorantesGonzalezJoseAntonio2324T4.java
@@ -1,5 +1,12 @@
 package cuenta;
 
+/**
+ * La clase CuentaDorantesGonzalezJoseAntonio2324T4 representa una cuenta
+ * bancaria con funcionalidades básicas como asignar un nombre, obtener el
+ * saldo, ingresar y retirar dinero, y obtener información sobre la cuenta.
+ * 
+ * @author Jose Antonio Dorantes Gonzalez
+ */
 public class CuentaDorantesGonzalezJoseAntonio2324T4 {
 
     private String nombre;
@@ -7,9 +14,26 @@ public class CuentaDorantesGonzalezJoseAntonio2324T4 {
     private double saldo;
     private double tipoInteres;
 
+    /**
+     * Constructor por defecto de la clase
+     * CuentaDorantesGonzalezJoseAntonio2324T4. Crea una instancia de cuenta con
+     * valores por defecto.
+     */
+    
     public CuentaDorantesGonzalezJoseAntonio2324T4() {
     }
 
+    /**
+     * Constructor parametrizado de la clase
+     * CuentaDorantesGonzalezJoseAntonio2324T4. Crea una instancia de cuenta con
+     * los valores especificados.
+     *
+     * @param nom Nombre del titular de la cuenta.
+     * @param cue Número de cuenta.
+     * @param sal Saldo inicial de la cuenta.
+     * @param tipo Tasa de interés de la cuenta.
+     */
+    
     public CuentaDorantesGonzalezJoseAntonio2324T4(String nom, String cue, double sal, double tipo) {
         nombre = nom;
         cuenta = cue;
@@ -17,18 +41,44 @@ public class CuentaDorantesGonzalezJoseAntonio2324T4 {
         tipoInteres = tipo;
     }
 
+    /**
+     * Asigna un nombre al titular de la cuenta.
+     *
+     * @param nom Nombre del titular de la cuenta.
+     */
+    
     public void asignarNombre(String nom) {
         setNombre(nom);
     }
 
+    /**
+     * Obtiene el nombre del titular de la cuenta.
+     *
+     * @return Nombre del titular de la cuenta.
+     */
+    
     public String obtenerNombre() {
         return getNombre();
     }
 
+    /**
+     * Obtiene el saldo actual de la cuenta.
+     *
+     * @return Saldo actual de la cuenta.
+     */
+    
     public double estado() {
         return getSaldo();
     }
 
+    /**
+     * Ingresa una cantidad de dinero en la cuenta.
+     *
+     * @param cantidad  Cantidad de dinero a ingresar.
+     * @param concepto  Concepto o descripción de la operación.
+     * @throws Exception Si la cantidad a ingresar es negativa.
+     */
+    
     public void ingresar(double cantidad, String concepto) throws Exception {
         if (cantidad < 0) {
             throw new Exception("No se puede ingresar una cantidad negativa");
@@ -36,6 +86,13 @@ public class CuentaDorantesGonzalezJoseAntonio2324T4 {
         setSaldo(getSaldo() + cantidad);
     }
 
+     /**
+     * Retira una cantidad de dinero de la cuenta.
+     *
+     * @param cantidad  Cantidad de dinero a retirar.
+     * @throws Exception Si la cantidad a retirar es negativa o si no hay suficiente saldo.
+     */
+    
     public void retirar(double cantidad) throws Exception {
         if (cantidad <= 0) {
             throw new Exception("No se puede retirar una cantidad negativa");
@@ -46,63 +103,97 @@ public class CuentaDorantesGonzalezJoseAntonio2324T4 {
         setSaldo(getSaldo() - cantidad);
     }
 
+     /**
+     * Obtiene el número de cuenta.
+     *
+     * @return Número de cuenta.
+     */
+    
     public String obtenerCuenta() {
         return getCuenta();
     }
 
     /**
-     * @return the nombre
+     * Obtiene el nombre del titular de la cuenta.
+     *
+     * @return Nombre del titular de la cuenta.
      */
+    
     public String getNombre() {
         return nombre;
     }
 
     /**
-     * @param nombre the nombre to set
+     * Establece el nombre del titular de la cuenta.
+     *
+     * @param nombre Nombre del titular de la cuenta.
      */
+    
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    /**
-     * @return the cuenta
+   /**
+     * Obtiene el número de cuenta.
+     *
+     * @return Número de cuenta.
      */
+    
     public String getCuenta() {
         return cuenta;
     }
 
     /**
-     * @param cuenta the cuenta to set
+     * Establece el número de cuenta.
+     *
+     * @param cuenta Número de cuenta.
      */
+    
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
     /**
-     * @return the saldo
+     * Obtiene el saldo actual de la cuenta.
+     *
+     * @return Saldo actual de la cuenta.
      */
+    
     public double getSaldo() {
         return saldo;
     }
 
-    /**
-     * @param saldo the saldo to set
+   /**
+     * Establece el saldo actual de la cuenta.
+     *
+     * @param saldo Saldo actual de la cuenta.
      */
+    
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
     /**
-     * @return the tipoInteres
+     * Obtiene la tasa de interés de la cuenta.
+     *
+     * @return Tasa de interés de la cuenta.
      */
+    
     public double getTipoInteres() {
         return tipoInteres;
     }
 
-    /**
-     * @param tipoInteres the tipoInteres to set
+     /**
+     * Establece la tasa de interés de la cuenta.
+     *
+     * @param tipoInteres Tasa de interés de la cuenta.
      */
+    
     public void setTipoInteres(double tipoInteres) {
         this.tipoInteres = tipoInteres;
+    }
+
+    void ingresar(int x) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
--- src/cuenta/Main.java
+++ src/cuenta/Main.java
@@ -1,7 +1,21 @@
 package cuenta;
 
+/**
+ * Clase main del programa, que define un objeto cuenta bancaria con unos
+ * parámetros y realiza acciones de ingresar y retirar dinero de dicha cuenta.
+ *
+ * @author Jose Antonio Dorantes Gonzalez
+ */
 public class Main {
 
+    /**
+     * Método main.
+     *
+     * Este método es el principal de ejecución del programa. Crea una cuenta
+     * bancaria, realiza algunas operaciones de ingreso y retiro de dinero, y
+     * muestra el saldo actual de la cuenta.
+     *
+     */
     public static void main(String[] args) {
         CuentaDorantesGonzalezJoseAntonio2324T4 miCuentaDorantesGonzalezJoseAntonio2324T4;
         double saldoActual;
@@ -16,21 +30,41 @@ public class Main {
 
         x = 100;                //Declaración dinero a ingresar
         ingresaDineroDorantesGonzalezJoseAntonio(x, miCuentaDorantesGonzalezJoseAntonio2324T4);
-        
+
         saldoActual = miCuentaDorantesGonzalezJoseAntonio2324T4.estado();
         System.out.println("El saldo actual es " + saldoActual);
     }
 
+    /**
+     * Método ingresaDineroDorantesGonzalezJoseAntonio.
+     *
+     * En este método se va a realizar la acción de ingresar cierta cantidad de
+     * dinero en la cuenta bancaria.
+     *
+     * @param x Cantidad de dinero a ingresar.
+     * @param miCuentaDorantesGonzalezJoseAntonio2324T4 La cuenta bancaria en la
+     * que se realizará el ingreso.
+     */
     private static void ingresaDineroDorantesGonzalezJoseAntonio(int x, CuentaDorantesGonzalezJoseAntonio2324T4 miCuentaDorantesGonzalezJoseAntonio2324T4) {
         try {
 
             System.out.println("Ingreso en cuenta de " + x + "€");
-            miCuentaDorantesGonzalezJoseAntonio2324T4.ingresar(x, (ERROR);
+            miCuentaDorantesGonzalezJoseAntonio2324T4.ingresar(x);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
     }
 
+    /**
+     * Método retiraDineroDorantesGonzalezJoseAntonio.
+     *
+     * En este método se va a realizar la acción de retirar cierta cantidad de
+     * dinero de la cuenta bancaria.
+     *
+     * @param x Cantidad de dinero a retirar.
+     * @param miCuentaDorantesGonzalezJoseAntonio2324T4 La cuenta bancaria de la
+     * que se retirará el dinero.
+     */
     private static void retiraDineroDorantesGonzalezJoseAntonio(int x, CuentaDorantesGonzalezJoseAntonio2324T4 miCuentaDorantesGonzalezJoseAntonio2324T4) {
         try {
             System.out.println("Retirada en cuenta de " + x + "€");
